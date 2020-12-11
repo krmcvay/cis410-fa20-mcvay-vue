a41e0d5108454655c9ef6fd6eead55020692741a
krmcvay <krmcvay@rams.colostate.edum>
2020-12-10 20:43:17 -0700

    12/10 Vuex store, sign in

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,11 +4,66 @@
         <div v-if="this.$route.query.signupsuccess" class="alert alert-success">
             Thanks for signing up. Please log in now. 
         </div>
+
+        <form @submit.prevent="onSubmit">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label> 
+                <input type="email" id="exampleInputEmail1" required="required" placeholder="Enter email"  class="form-control" v-model="email">
+            </div>
+
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" id="exampleInputPassword1" placeholder="Enter password" required="required"  class="form-control" v-model="password">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+            <p v-if="credentialsError" class="form-text text-danger">Invalid Credentials</p>
+
+            <p v-if="loginError" class="form-text text-danger">Could not log you in. Please try again later.</p>
+        </form>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+    data(){
+        return{
+            email:'',
+            password:'',
+            loginError: false,
+            credentialsError: false
+        }
+    },
+    methods:{
+        onSubmit(){
+            const myFormData={
+                email: this.email,
+                password: this.password
+            };
+            //console.log("form-data:", myFormData)
+
+            axios.post("/customers/login", myFormData)
+            .then(myResponse=>{
+                console.log("Here is my response", myResponse)
+
+                this.$store.commit("storeTokenInApp", myResponse.data.token);
+
+                this.$store.commit("storeUserInApp", myResponse.data.user);
+
+                this.$router.replace("/account")
+            })
+            .catch((myError)=>{
+                console.log("my error", myError.response.data)
+
+                if(myError.response.data == 'Invalid user credentials'){
+                    this.credentialsError = true
+                }
+                else{this.loginError = true}
+            })
+        }
+    }
     
 }
 </script>
