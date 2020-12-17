cc80ad887eaec4e2e8eeb9f5139482455a35c786
krmcvay <krmcvay@rams.colostate.edum>
2020-12-16 19:19:47 -0700

    12/16 /bookDetail

--- src/components/BookDetail.vue
+++ src/components/BookDetail.vue
@@ -1,12 +1,44 @@
 <template>
     <div>
-        <h1>Book Details</h1>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="text-primary">{{book.Title}}</h2>
+                <br/>
+                <p>Author: <br/> <strong>{{book.Author}}</strong></p>
+                <p>Genre: <br/> <strong>{{book.GenreName}}</strong></p>
+                <p>Price: <br/> <strong>{{formattedPrice}}</strong></p>
+                <p>Publisher: <br/> <strong>{{book.Publisher}}</strong></p>
+                <p>ISBN: <br/> <strong>{{book.ISBN}}</strong></p>
+            </div>
+        </div>
+        <br/>
+        <router-link v-if="auth" :to="`/books/${this.$route.params.pk}/purchase`">
+        <button type="button" class="bth btn-success">Purchase this Book</button>
+        </router-link>
+        <router-link v-else :to="`/login`">
+        <button type="button" class="bth btn-outline-success">Log In to Purchase</button>
+        </router-link>
     </div>
 </template>
 
 <script>
 export default {
-    
+    computed:{
+        book(){
+            var books = this.$store.state.books;
+            var thisBook = books.find((aBook)=> aBook.BookPK == this.$route.params.pk)
+
+            //console.log("here is the book you want", thisBook);
+            return thisBook
+        },
+        formattedPrice(){
+            return new Intl.NumberFormat("en-US",{
+            style: 'currency',
+            currency: 'USD',
+            }).format(this.book.Price)
+        },
+        auth(){return this.$store.state.token}
+    }
 }
 </script>
 
